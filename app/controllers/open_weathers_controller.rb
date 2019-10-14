e1e2dcfc6d8a582b467e92c157b22bc78139dc1c
jonmccarthy7 <jtmcc93@gmail.com>
2019-10-13 22:59:02 -0700

    serializer attempt

--- app/controllers/open_weathers_controller.rb
+++ app/controllers/open_weathers_controller.rb
@@ -1,10 +1,9 @@
 class OpenWeathersController < ApplicationController
     include TempConversionHelper
 
-
     def index
         result = RequestOpenWeatherApi.new(params[:zip]).call
-        result = ::OpenWeatherSerializer.serialize(result)
+        result = Serializers::OpenWeatherSerializer.serialize(result)
         render json: result
     end
 
