32e60786f54604199edad2311f510a4b6123a14e
jonmccarthy7 <jtmcc93@gmail.com>
2019-10-13 23:47:14 -0700

    another try

--- config/application.rb
+++ config/application.rb
@@ -30,8 +30,10 @@ module WeatherApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.enable_dependency_loading = true
+    config.autoload_paths << Rails.root.join('lib')
     Dir.glob(Rails.root.join('app/serializers/**/*.rb')).each { |f| require f }
-    
+
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
